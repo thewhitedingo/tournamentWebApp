9e4ac72e106ecf8992d5502818a296b25ba10495
Tyler Despain <tylermdespain@gmail.com>
2016-03-25 08:29:25 -0700

    Set up database
    set up the database, created tables and added some basic add and delete
    functions

---
+++ tournament.sql
@@ -0,0 +1,15 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+
+CREATE TABLE players (id SERIAL PRIMARY KEY,
+					  name TEXT);
+
+CREATE TABLE matches (matchNum SERIAL,
+					  winner INTEGER REFERENCES players (id),
+					  loser INTEGER REFERENCES players (id));
